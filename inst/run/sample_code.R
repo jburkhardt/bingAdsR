af315c66c1deae5208a3639baad17e2e91fd4cac
jburkhardt <johannes.burkhardt@gmail.com>
2019-08-21 16:49:41 +0200

    migrate package towards new bing api version v13

---
+++ inst/run/sample_code.R
@@ -0,0 +1,36 @@
+rm(list=ls())
+# library(devtools)
+# install_github("jburkhardt/bingAdsR")
+library(bingAdsR)
+
+# Credentials ---------------------------------------------------
+credentials <- list(
+  ###For authorization and access token###
+  client_id = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  client_secret = "XXXXXXXXXXXXXXX",
+  ###To get data###
+  username = "abc@abc.com",
+  password = "***********",
+  developer_token = "XXXXXXXXXXXXXXXXX",
+  customer_id = "XXXXXXXXXX",
+  account_id = "XXXXXXX"
+)
+
+# Authentication -----------------------------------------------
+credentials <- baAuthentication(credentials)
+
+# Load data ----------------------------------------------------
+startDate <- "2018-05-17"
+endDate <- "2018-05-17"
+
+report <- "CampaignPerformanceReport"
+columns <- c("AccountName", "CampaignName", "TimePeriod", "Impressions", "Clicks", "Spend", "Conversions")
+
+reportId <- getReportId(credentials, report, columns, startDate, endDate)
+downloadUrl <- getDownloadUrl(credentials, reportId)
+df <- getDataFromURL(downloadUrl)
+
+
+report <- "AccountPerformanceReport"
+columns <- c("AccountName", "DeviceType", "TimePeriod", "Impressions", "Clicks", "Spend", "Conversions")
+
